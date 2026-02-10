334513f1a9c140ac837afbde13859a5acc99c958
bolicos <apollobolico@gmail.com>
2026-02-09 22:29:04 -0300

    feature: Initial commit 3

--- src/components/shared/Section.tsx
+++ src/components/shared/Section.tsx
@@ -1,4 +1,11 @@
-export function Section({ title, children }) {
+import type { JSX } from "react";
+
+type SectionProps = {
+  title?: string;
+  children?: JSX.Element;
+};
+
+export function Section({ title, children }: SectionProps) {
   return (
     <section className="py-12 px-4 max-w-6xl mx-auto">
       <h2 className="text-2xl font-bold mb-6">{title}</h2>
